9a667ae53dd2736759b5ba68e4690aaa4a6f263e
sirrodgepodge <rwcbeaman@gmail.com>
2015-04-18 05:38:07 +0000

    further optimized

--- montyHall.js
+++ montyHall.js
@@ -1,17 +1,8 @@
 var montyHall = function (numTimes) {
     var result = 0;
-    var doorPick = function(num){
-	if(num <= (1/3)) {
-	    return 1;
-	} else if (num <= (2/3)) {
-	    return 2;
-	} else {
-	    return 3;
-	}
-    };
     for (var i = 0; i < numTimes; i++) {
-	var prizeDoor = doorPick(Math.random());
-	var pick1 = doorPick(Math.random());
+	var prizeDoor = Math.floor(Math.random()*3);
+	var pick1 = Math.floor(Math.random()*3);
 	result += (pick1 === prizeDoor);
     }
     result = result/numTimes*100
