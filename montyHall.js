3719f85ac48072f012f02f1f50f2115b6b411154
sirrodgepodge <rwcbeaman@gmail.com>
2015-04-18 05:13:22 +0000

    rough draft

---
+++ montyHall.js
@@ -0,0 +1,21 @@
+var montyHall = function (numTimes, switched) {
+    var result = 0;
+    var doorPick = function(num){
+	if(num <= (1/3)) {
+	    return 1;
+	} else if (num <= (2/3)) {
+	    return 2;
+	} else {
+	    return 3;
+	}
+    };
+    for (var i = 0; i < numTimes; i++) {
+	var prizeDoor = doorPick(Math.random()) += 1;
+	var pick1 = doorPick(Math.random());
+	result += (switched && pick1 === prizeDoor) || (!switched && pick1 !== prizeDoor);
+    }
+    return result/numTimes*100 + "% won";
+}
+
+console.log("stayed: " + montyHall(1000, false));
+console.log("switched: " + montyHall(1000, true));
